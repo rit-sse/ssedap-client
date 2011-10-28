fe7a5ac83b8cb51a725f048f2ef75383a18c28a0
Sean Barkley <sean@seanbarkley.com>
2011-10-28 15:55:17 -0400

    Added functionality and added a test file #WORKING

--- lib/ssedap-client.rb
+++ lib/ssedap-client.rb
@@ -1,13 +1,37 @@
 require "ssedap-client/version"
+require "patron"
 
 module SSEDAP
   module Client
-    def self.authorize
+    def self.authorize(username, password)
       # code...
+      information = Hash.new
+      sess = Patron::Session.new
+      sess.timeout = 10
+      sess.base_url = "http://localhost:3000"
+      sess.headers['User-Agent'] = 'ssedap/1.0'
+      info = sess.post("/api/authorize", "username=#{username}&password=#{password}")
+      information["status"] = info.status
+      information["info"] = info.body
+      #puts info.status
+      #puts info.body
+      return information
     end
 
-    def self.userinfo
+    def self.userinfo(username, password, lookup)
       # code...
+      information = Hash.new
+      sess = Patron::Session.new
+      sess.timeout = 10
+      sess.base_url = "http://localhost:3000"
+      sess.headers['User-Agent'] = 'ssedap/1.0'
+      info = sess.post("/api/userinfo", 
+                       "username=#{username}&password=#{password}&lookup=#{lookup}")
+      information["status"] = info.status
+      information["info"] = info.body
+      puts info.status
+      puts info.body
+      return information
     end
   end
 end
